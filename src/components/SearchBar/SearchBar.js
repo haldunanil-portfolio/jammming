fdcac706d26cba282faba7a58eabf07402c37448
Haldun Anil <haldunanil@gmail.com>
2018-04-01 21:45:33 -0400

    Create functionality for SearchBar
    Currently, searches are logged to console but the infrastructure in place for API calls. Enter/Return to submit also working

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,11 +2,35 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { term: '' };
+		
+		this.search = this.search.bind(this);
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleKeyDown = this.handleKeyDown.bind(this);
+	}
+
+	search() {
+		this.props.onSearch(this.state.term);
+	}
+
+	handleTermChange(event) {
+		this.setState({ term: event.target.value });
+	}
+
+	handleKeyDown(event) {
+		// 'keypress' event misbehaves on mobile so we track 'Enter' key via 'keydown' event
+		if (event.key === 'Enter') {
+			this.search();
+		}
+	}
+
 	render() {
 		return (
-			<div className="SearchBar">
-			  <input placeholder="Enter A Song, Album, or Artist" />
-			  <a>SEARCH</a>
+			<div className="SearchBar" onKeyDown={this.handleKeyDown}>
+			  <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
+			  <a onClick={this.search}>SEARCH</a>
 			</div>		
 		);
 	}
